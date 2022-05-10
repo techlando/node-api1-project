07f732e6704ee8f411cbbc315ba2931c3fb61872
Landon Phillips <landonphillips@Landons-MBP.lan1>
2022-05-10 09:33:49 -0500

    fetching users

--- api/server.js
+++ api/server.js
@@ -1,12 +1,39 @@
 // BUILD YOUR SERVER HERE
 
 const express = require('express')
-
+const User = require('./users/model')
 const server = express();
 
 server.get('/api/users', (req, res) => {
-    res.json("users")
+   User.find()
+   .then(users => {
+       res.json(users)
+   })
+       .catch(err => {
+           res.status(500).json({
+               messsage: 'error getting users',
+               err: err.message,
+               stack: err.stack
+           })
+       })
+   
 })
+
+
+server.get('/api/users/:id', (req, res) => {
+    User.findById(req.params.id)
+    .then(user => {
+        res.json(user)
+    })
+        .catch(err => {
+            res.status(500).json({
+                messsage: 'error getting users',
+                err: err.message,
+                stack: err.stack
+            })
+        })
+    
+ })
 
 server.use('*', (req, res) => {
     res.status(404).json({
