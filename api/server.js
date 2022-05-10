ab227a92fe363e6ac88b8bc55285cdd7609c6016
Landon Phillips <landonphillips@Landons-MBP.lan1>
2022-05-10 09:09:43 -0500

    second commit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,9 @@ const express = require('express')
 
 const server = express();
 
+server.get('/api/users', (req, res) => {
+    res.json("users")
+})
 
 server.use('*', (req, res) => {
     res.status(404).json({
